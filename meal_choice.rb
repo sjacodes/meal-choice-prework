2252b0f4745f9604e1c5a5bddf6ae9a71a1163ff
Sarah Jacob <smjacob91@gmail.com>
2018-07-01 11:39:33 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,4 +1,15 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
+def meal_choice
 
+
+describe '#meal_choice' do
+  it 'should default to meat' do
+    expect(meal_choice).to eq("meat")
+  end
+
+  it 'should allow you to set a meal' do
+    expect(meal_choice("vegan")).to eq("vegan")
+  end
+end
