fde7f308cdea85d4b61a5094e514b172337751d9
Sarah Jacob <smjacob91@gmail.com>
2018-07-01 11:43:02 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,15 +2,5 @@
 # passed into it and defaults to meat.
 
 def meal_choice(meal = meat)
-end
-
-
-describe '#meal_choice' do
-  it 'should default to meat' do
-    expect(meal_choice).to eq("meat")
-  end
-
-  it 'should allow you to set a meal' do
-    expect(meal_choice("vegan")).to eq("vegan")
-  end
+  puts #{meal}
 end
